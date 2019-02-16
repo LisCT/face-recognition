874c0900422c714fa41c511c0e9b4d78cc5ba3b2
Lisbel Cruz <Lcruztaveras@gmail.com>
2019-02-15 20:01:58 -0600

    preparint ot connect the server with our front-ed

--- node/server.js
+++ node/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
 
 /*
     END POINTS
@@ -21,7 +22,6 @@ const database = {
             id: '123',
             name: 'John',
             email: 'john@gmail.com',
-            password: 'cookies',
             entries: 0,
             joined: new Date()
         },
@@ -29,9 +29,15 @@ const database = {
             id: '1234',
             name: 'Sally',
             email: 'sally@gmail.com',
-            password: 'bananas',
             entries: 0,
             joined: new Date()
+        }
+    ],
+    login: [
+        {
+            id: '908',
+            has: '',
+            email: 'john@gmail.com'
         }
     ]
 };
@@ -134,6 +140,20 @@ app.put('/image', (req, res) => {
     return false;
 
 });
+
+// REGISTER
+// bcrypt.hash('bacon', null, null, (err, hash) => {
+//     // Store hash in your password DB.
+// });
+
+// SIGN IN
+// // Load hash from your password DB.
+// bcrypt.compare('bacon', hash, (err, res) => {
+//     // res == true
+// });
+// bcrypt.compare('veggies', hash, (err, res) => {
+//     // res = false
+// });
 
 app.listen(3000, () => {
 
